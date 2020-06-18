eada67f8287344ab8af6255aa721fa7b22e2af42
Evellyn Costa <ecosta.inf@gmail.com>
2020-06-17 23:33:27 -0300

    testando duas origens

--- src/main/java/com/pagea/resources/ResponsavelResource.java
+++ src/main/java/com/pagea/resources/ResponsavelResource.java
@@ -1,8 +1,12 @@
 package com.pagea.resources;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
+
+import com.pangea.repository.ResponsavelRepository;
 
 @Path("/responsaveis")
 public class ResponsavelResource {
-
+	@Inject
+	ResponsavelRepository responsavelRespository;
 }
